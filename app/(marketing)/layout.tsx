d76097691c089d065b4661491ebeb3e219e95dd1
Yatharth Singh <128090899+yatharth1444@users.noreply.github.com>
2025-09-01 22:04:52 +0530

    Add files via upload

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,15 @@
+import { Navbar } from "@/components/marketing/navbar";
+import React from "react";
+
+export default function MarketingLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="h-full dark:bg-[#1F1F1F]">
+      <Navbar />
+      <main className="h-full pt-40">{children}</main>
+    </div>
+  );
+}
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,15 @@
+import { Heading } from "@/components/marketing/heading";
+import { Heroes } from "@/components/marketing/heroes";
+import { Footer } from "@/components/marketing/footer";
+
+export default function MarketingPage() {
+  return (
+    <div className="min-h-full flex flex-col dark:bg-[#1F1F1F]">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading />
+        <Heroes />
+      </div>
+      <Footer />
+    </div>
+  );
+}
